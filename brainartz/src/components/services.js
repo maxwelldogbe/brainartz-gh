0937db5f3b23f8f25fdda0ed2bea2f152b215b71
maxwelldogbe <monstervibesq@gmail.com>
2023-09-07 08:44:47 +0000

    my current commit

--- brainartz/src/components/services.js
+++ brainartz/src/components/services.js
@@ -1,7 +1,14 @@
 function Service() {
        return (
          <>
-            <h1>Service</h1>
+            <div>
+               <section className="textiles">
+                  <h3>Textiles</h3>
+               </section>
+               <section className="branding">
+                  <h3></h3>
+               </section>
+            </div>
          </>
      );
      }
